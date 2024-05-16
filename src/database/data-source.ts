6501c028400ec72a0b9ed8330835728a875be294
NohWookJin <jinn98@kakao.com>
2024-05-17 02:21:05 +0900

    fix: env path fix

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,12 @@
 import { DataSource } from 'typeorm'
 import { config } from 'dotenv'
 
-config({ path: '.env.local' })
+config({ path: '.env.production' })
 
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
+  port: 5432,
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
