f19be7da7cb5ae88f9e032aa50b328ca2f967a3f
NohWookJin <jinn98@kakao.com>
2024-05-20 00:38:37 +0900

    fix: routine deletes error

--- src/routes/routine/routine.module.ts
+++ src/routes/routine/routine.module.ts
@@ -4,9 +4,12 @@ import { RoutineService } from './routine.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Routine } from 'src/entity/routine.entity'
 import { User } from 'src/entity/user.entity'
+import { Todo } from 'src/entity/todo.entity'
+import { Analysis } from 'src/entity/analysis.entity'
+import { Blog } from 'src/entity/blog.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Routine, User])],
+  imports: [TypeOrmModule.forFeature([Routine, User, Todo, Blog, Analysis])],
   controllers: [RoutineController],
   providers: [RoutineService]
 })
