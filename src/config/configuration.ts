43730c968d3817627b95d0bd55e336e0aa90edc8
NohWookJin <jinn98@kakao.com>
2024-06-06 21:25:40 +0900

    Fix: File Upload

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,5 +4,9 @@ export default () => ({
   DB_PORT: process.env.DB_PORT,
   DB_NAME: process.env.DB_NAME,
   DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  AWS_REGION: process.env.AWS_REGION,
+  AWS_S3_ACCESS_KEY: process.env.AWS_S3_ACCESS_KEY,
+  AWS_S3_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  AWS_S3_BUCKET_NAM: process.env.AWS_S3_BUCKET_NAM
 })
